ce538e25bbe681adbb256286f4e427946cf5747e
Tadeu Neto <tadeubranconeto@gmail.com>
2024-01-30 11:33:46 -0300

    Add comentários
    Adicionei comentários ao código para ter um melhor entendimento quando precisar analisar o código.
    
    Exclui alguns 'console.log' que era usado apenas para verificação de funcionamento. Como não utilizo mais, o exclui

--- buscaCep/script.js
+++ buscaCep/script.js
@@ -1,3 +1,4 @@
+// variáveis
 const cep = document.querySelector('#cep')
 const rua = document.querySelector('#rua')
 const bairro = document.querySelector('#bairro')
@@ -5,23 +6,24 @@ const cidade = document.querySelector('#cidade')
 const uf = document.querySelector('#uf')
 const ibge = document.querySelector('#ibge')
 
+// função que irá pesquisar o cep
 function pesquisaCep(valor) {
+ 
   //expressao regular para validar o cep
-  const somenteNumeros = valor.replace(/\D/g, '')
   const cepValid = /^[0-9]{8}$/
-  console.log(cep.value)
+// se o cep digitado for válido, ou seja, tiver 8 caracteres numéricos o if será executado
   if (cepValid.test(cep.value)) {
     document.getElementById('rua').value = '...'
     document.getElementById('bairro').value = '...'
     document.getElementById('cidade').value = '...'
     document.getElementById('uf').value = '...'
     document.getElementById('ibge').value = '...'
-
-    const script = document.createElement('script')
-
-    script.src =
-      'https://viacep.com.br/ws/' + cep.value + '/json/?callback=meu_callback'
-
+// cria um elemento script dinâmico
+  const script = document.createElement('script')
+// define o atributo src do elemento script com a URL da API DO vIA cEP, onde cep.value é o valor do CEP inserido e meu_callback é um callback que será chamado quando os dados estiverem disponíveis.
+    script.src ='https://viacep.com.br/ws/' + cep.value + '/json/?callback=meu_callback'
+    
+// adiociona o elemnto scritp criado ao HTML iniciando a requisição para a API
     document.body.appendChild(script)
   } else {
     alert('formato invalido')
@@ -29,7 +31,6 @@ function pesquisaCep(valor) {
 }
 
 function meu_callback(conteudo) {
-  console.log(conteudo)
   if (!('error' in conteudo)) {
     document.getElementById('rua').value = conteudo.logradouro
     document.getElementById('bairro').value = conteudo.bairro
