dabf40b629d2ca7c8ad4ab36ebdeba46cafde595
Tadeu Neto <tadeubranconeto@gmail.com>
2024-02-14 15:22:09 -0300

    ajustes css ejs
    no js inclui a função onde o usuario também pode fazer a busca com um ENTER e nao somente com o tab
    
    ajustes de estilo no css

--- buscaCep/script.js
+++ buscaCep/script.js
@@ -7,23 +7,30 @@ const uf = document.querySelector('#uf')
 const ibge = document.querySelector('#ibge')
 
 // função que irá pesquisar o cep
+
+function verificarEnter(event) {
+  if (event.key === 'Enter') {
+      pesquisaCep(document.getElementById('cep').value);
+  }
+}
+
 function pesquisaCep(valor) {
- 
   //expressao regular para validar o cep
   const cepValid = /^[0-9]{8}$/
-// se o cep digitado for válido, ou seja, tiver 8 caracteres numéricos o if será executado
+  // se o cep digitado for válido, ou seja, tiver 8 caracteres numéricos o if será executado
   if (cepValid.test(cep.value)) {
     document.getElementById('rua').value = '...'
     document.getElementById('bairro').value = '...'
     document.getElementById('cidade').value = '...'
     document.getElementById('uf').value = '...'
     document.getElementById('ibge').value = '...'
-// cria um elemento script dinâmico
-  const script = document.createElement('script')
-// define o atributo src do elemento script com a URL da API DO vIA cEP, onde cep.value é o valor do CEP inserido e meu_callback é um callback que será chamado quando os dados estiverem disponíveis.
-    script.src ='https://viacep.com.br/ws/' + cep.value + '/json/?callback=meu_callback'
-    
-// adiociona o elemnto scritp criado ao HTML iniciando a requisição para a API
+    // cria um elemento script dinâmico
+    const script = document.createElement('script')
+    // define o atributo src do elemento script com a URL da API DO vIA cEP, onde cep.value é o valor do CEP inserido e meu_callback é um callback que será chamado quando os dados estiverem disponíveis.
+    script.src =
+      'https://viacep.com.br/ws/' + cep.value + '/json/?callback=meu_callback'
+
+    // adiociona o elemnto scritp criado ao HTML iniciando a requisição para a API
     document.body.appendChild(script)
   } else {
     alert('formato invalido')
